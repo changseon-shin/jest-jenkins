284654caa1bc7ae8922199541413d08e893f47ae
cs.shin <cs.shin@yanolja.com>
2020-02-18 13:34:31 +0900

    for jest jenkins integration (fail) test

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -10,7 +10,7 @@ describe('Pages', () => {
     describe('Index', () => {
         it('should render without throwing an error', function () {
             const wrap = mount(<IndexPage/>);
-            expect(wrap.find('div').text()).toBe('Hello World!');
+            expect(wrap.find('div').text()).toBe('Hello World');
         })
     })
 });
